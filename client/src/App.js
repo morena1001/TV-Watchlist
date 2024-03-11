f2ec66b981d1701c771f10fcfe6450f9edcc7d44
josue-flores03 <josue.flores03@utrgv.edu>
2024-03-11 18:22:57 -0500

    Setup

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' exact Component={Home} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
